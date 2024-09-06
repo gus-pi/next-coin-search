4226e012a2f2bcb2bb430330a82b7250c773d10e
PieMenard <pinedo.m.gustavo@gmail.com>
2024-09-05 22:48:56 -0400

    get coins in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,18 @@ import { Coin } from '@/types/Coin';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [coins, setCoins] = useState<Coin[]>([]);
+  useEffect(() => {
+    const getCoins = async () => {
+      const response = await fetch('/api/coins');
+      const data = await response.json();
+      setCoins(data.data.coins);
+    };
+    getCoins();
+  }, []);
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="text-center">
+      <h1 className="font-bold text-6xl mt-14">Crypto Coins</h1>
     </div>
   );
 }
